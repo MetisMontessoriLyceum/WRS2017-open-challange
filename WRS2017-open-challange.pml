57559f9130324020694e2da706c34bbd091192e1
Noah Loomans <noahloomans@gmail.com>
2017-08-06 15:40:02 +0900

    Initial Commit

---
+++ WRS2017-open-challange.pml
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="WRS2017-open-challange" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="metis-pepper" src="metis-pepper.png" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ translations/translation_en_US.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Sorry, I could not find anything about that.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Sorry, I could not find anything about that.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>What should I remind you of?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">What should I remind you of?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>When should I remind you of it?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">When should I remind you of it?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Here is your reminder?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Here is your reminder?</translation>
+        </message>
+    </context>
+</TS>
